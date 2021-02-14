9b41fb4e181f2215708cee62de3f075fb1fb4d4f
Ibukun Dairo <ibukundairo@hackerbay.io>
2021-02-14 14:54:48 +0100

    remove unused deps

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
-import { reducer as form } from "redux-form";
 import user from "./user";
 import category from "./category";
 import business from "./business";
 
 export default combineReducers({
-  form,
   user,
   category,
   business
